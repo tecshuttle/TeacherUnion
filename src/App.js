a5419c4bac415383d97de9a4e6d9bb399508181f
tom.xie <tecshuttle@gmail.com>
2024-12-20 08:38:26 +0800

    简单修改首页

--- src/App.js
+++ src/App.js
@@ -5,18 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p> 百度地图 </p>
+        <p> 站点管理 </p>
       </header>
     </div>
   );
